e493dbee8052659d38a9d211239d202b54a7cc16
kafumuleon <kafumuleon@gmail.com>
2023-03-16 05:19:10 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-\
+10-19\n");
+	return (1);
+}
