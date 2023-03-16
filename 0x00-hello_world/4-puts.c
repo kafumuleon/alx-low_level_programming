065b6adc53f155e49a7d8b8e3814ed7e0eabe5cb
kafumuleon <kafumuleon@gmail.com>
2023-03-16 04:18:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A programm that prints a line wiith puts function
+ *
+ * Return: (0) (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+	return (0);
+}
