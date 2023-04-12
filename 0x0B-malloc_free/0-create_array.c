61853ed3b43df650cd65d0544bfd453ee459cd46
kafumuleon <kafumuleon@gmail.com>
2023-04-12 06:47:20 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,20 +9,20 @@
 *
 * Return: a pointer to the array, or NULL if it fails
 */
-char *create_arrray(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
-char *array;
-unsigned int i;
+	char *array;
+	unsigned int i;
 
-if (size == 0)
-return (NULL);
+	if (size == 0)
+	return (NULL);
 
-array = (char *)malloc(sizeof(char) * size);
-if (array == NULL)
-return (NULL);
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+	return (NULL);
 
-for (i = 0; i < size; i++)
-array[i] = c;
+	for (i = 0; i < size; i++)
+	array[i] = c;
 
-return (array);
+	return (array);
 }
