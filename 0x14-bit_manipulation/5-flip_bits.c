e8d3357a1d07aafc473fde396b099f8e9e8229db
MemoAlya <memorygwanunga@gmail.com>
2023-05-08 13:44:51 -0700

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * flip_bits - number of bits you would need to flip to get from one to another
- * @n: num to flip
- * @m: number 2
- * Return: unsigned int
- */
+* flip_bits - number of bits you would need to flip to get form one to another
+*@n: num to flip
+*@m: number 2
+*Return: unsigned int
+*/
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
@@ -18,7 +18,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 			flips++;
 
 		n >>= 1;
-		m >>=1;
+		m >>= 1;
 		i++;
 	}
 	return (flips);
